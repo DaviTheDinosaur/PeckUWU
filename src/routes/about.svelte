6127dbbdae951dc3db5b9905a45296ca5c02db30
Elo Colburn <62953415+Ewie21@users.noreply.github.com>
2022-12-07 23:42:01 -0800

    wrote manger verification system

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -8,7 +8,7 @@
 	<ul>
 		<li>Create 'progress bars' for every department per robot</li>
 		<li>incluing checkpoints along it so people knowo what their goals are, and deadlines so people know how much time they have</li>
-		<li>Allow managers to sign in in order to add checkpoint</li>
+		<li>Allow managers to sign in in or`der to add checkpoint</li>
 		<li>Allow multi and cross-department taskflow lines, so everyone knows when robots get handed over to other departments</li>
 		<li>Display the website on a screen in Houston, The Lego Lab, and/or The Fab Lab, for everyone to see all the time(kind of like CLUCK but cooler)</li>
 	</ul>
